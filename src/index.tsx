2fefdde7e8c72dde7cd94f942511298bd8dc6325
Oisin Larkin <liballoisinl@gmail.com>
2025-06-23 17:55:16 +0100

    Added Upcoming movies page.

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import MoviePage from "./pages/movieDetailsPage";
 import MovieReviewPage from "./pages/movieReviewPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage";
 import SiteHeader from './components/siteHeader'
+import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
         <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
+        <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
