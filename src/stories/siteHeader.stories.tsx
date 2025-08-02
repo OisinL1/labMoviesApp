c594e086f8b2d769b112a6fa046affd8868f404e
Oisin Larkin <liballoisinl@gmail.com>
2025-08-02 13:50:52 +0100

    Fix broken stories in Storybook.

---
+++ src/stories/siteHeader.stories.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import SiteHeader from "../components/siteHeader";
+import { MemoryRouter } from "react-router";
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: "App Header",
+  component: SiteHeader,
+  decorators: [
+    (Story: React.FC) => <MemoryRouter initialEntries={["/"]}><Story /></MemoryRouter>,
+  ],
+} satisfies Meta<typeof SiteHeader>;
+
+export default meta;
+
+type Story = StoryObj<typeof SiteHeader>;
+
+export const Basic: Story = { args: {} };
