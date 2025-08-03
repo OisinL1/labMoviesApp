613b8a3a8c8a64bd27dda05163499b20408802de
Oisin Larkin <liballoisinl@gmail.com>
2025-08-03 15:34:36 +0100

    Fix upcoming movies page.

--- src/pages/upcomingMoviesPage.tsx
+++ src/pages/upcomingMoviesPage.tsx
@@ -1,21 +1,46 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import PageTemplate from "../components/templateMovieListPage";
-import { BaseMovieProps } from "../types/interfaces";
-import { getUpcomingMovies } from "../api/tmdb-api"; 
+import { useQuery } from "react-query";
+import { getUpcomingMovies } from "../api/tmdb-api";
+import { DiscoverMovies } from "../types/interfaces";
+import Spinner from "../components/spinner";
+import useFiltering from "../hooks/useFiltering";
+import MovieFilterUI, { titleFilter, genreFilter } from "../components/movieFilterUI";
+
+const titleFiltering = { name: "title", value: "", condition: titleFilter };
+const genreFiltering = { name: "genre", value: "0", condition: genreFilter };
 
 const UpcomingMoviesPage: React.FC = () => {
-  const [movies, setMovies] = useState<BaseMovieProps[]>([]);
+  const { data, error, isLoading, isError } = useQuery<DiscoverMovies, Error>("upcoming", getUpcomingMovies);
+  const { filterValues, setFilterValues, filterFunction } = useFiltering([titleFiltering, genreFiltering]);
 
-  useEffect(() => {
-    getUpcomingMovies().then(setMovies);
-  }, []);
+  if (isLoading) return <Spinner />;
+  if (isError) return <h1>{error.message}</h1>;
+
+  const movies = data ? data.results : [];
+  const displayedMovies = filterFunction(movies);
+
+  const changeFilterValues = (type: string, value: string) => {
+    const changedFilter = { name: type, value: value };
+    const updatedFilterSet = type === "title"
+      ? [changedFilter, filterValues[1]]
+      : [filterValues[0], changedFilter];
+    setFilterValues(updatedFilterSet);
+  };
 
   return (
-    <PageTemplate
-      title="Upcoming Movies"
-      movies={movies}
-      selectFavourite={() => {}} 
-    />
+    <>
+      <PageTemplate
+        title="Upcoming Movies"
+        movies={displayedMovies}
+        action={() => null}
+      />
+      <MovieFilterUI
+        onFilterValuesChange={changeFilterValues}
+        titleFilter={filterValues[0].value}
+        genreFilter={filterValues[1].value}
+      />
+    </>
   );
 };
 
